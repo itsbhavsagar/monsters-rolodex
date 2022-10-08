b7f9d78a70da285964d386dde1b997b74167ee23
itsbhavsagar <46781035+itsbhavsagar@users.noreply.github.com>
2022-10-08 08:36:31 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monster1: {
+      monsters: [ 
+        {
         name: 'Linda',
       },
-      monster2: {
+      {
         name: 'Frank',
       },
-      monster3: {
+      {
         name: 'Jacky',
       },
+    ]
+      
     };
   }
   render() {
     return (
       <div className='App'>
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
+        {
+          this.state.monsters.map((monster) => {
+             return <h1>{monster.name} </h1>
+          })
+        }
       </div>
     );
   }
