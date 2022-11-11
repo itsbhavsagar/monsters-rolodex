ab7518b744f3689a3805c67bc98d27938302b3c1
Bhavsagar <bhavsagar@protonmail.com>
2022-11-11 18:30:14 +0530

    Added Search Functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: ''
     };
     console.log('constructor');
   }
@@ -30,6 +31,14 @@ class App extends Component {
 
   render() {
     console.log('render');
+
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+  
+
+
     return (
       <div className="App">
         <input
@@ -38,19 +47,13 @@ class App extends Component {
           placeholder="search monsters"
           onChange={(event) => {
             console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name
-                .toLocaleLowerCase()
-                .includes(searchString);
-            });
+            const searchField = event.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return { monsters: filteredMonsters };
+              return { searchField };
             });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
